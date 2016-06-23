94f825e4c579ddb4f577858e8bec3d37aed90fef
Rameshwar Bhaskaran <rameshwar.zorro@gmail.com>
2016-06-24 00:37:36 +0530

    Worked on fragment

--- app/src/main/java/com/summerdeveloper/rameshwar/quizzardo/QuizFragment.java
+++ app/src/main/java/com/summerdeveloper/rameshwar/quizzardo/QuizFragment.java
@@ -1,14 +1,43 @@
 package com.summerdeveloper.rameshwar.quizzardo;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by rameshwar on 23/6/16.
  */
 public class QuizFragment extends Fragment {
 
-    public static QuizFragment createInstance()
+    TextView tvQuestion;
+    EditText etAnswer;
+    Button button;
+   public QuizFragment()
+   {
+
+   }
+
+    public static QuizFragment createNewInstance()
     {
-        return null;
+        return new QuizFragment();
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.quiz_fragment, container, false);
+        tvQuestion=(TextView)view.findViewById(R.id.tvQuestion);
+        etAnswer=(EditText)view.findViewById(R.id.etAnswer);
+        button=(Button)view.findViewById(R.id.button);
+        return view;
+    }
+
+
+
 }
